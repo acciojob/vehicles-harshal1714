83905a30087be361963644243759d91ad6218ff3
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-02-11 16:55:50 +0530

    my 3rd commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,13 +1,21 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
-        return this.getVehicleName();
+        return name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return this.getVehicleCapacity();
+        return capacity;
     }
 }
