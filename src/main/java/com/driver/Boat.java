26572ffdf735ba3934c6d6a2c4a3b970c82efd13
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-02-11 13:59:17 +0530

    my 1st commit

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    @Override
+    public String getVehicleName() {
+        return this.getVehicleName();
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.getVehicleCapacity();
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,9 +2,11 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual) {
+   public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+      super("alto",4,"suv",6,4,isManual,1,7);
     }
+
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
@@ -20,9 +22,29 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+        //r    super.changeGear=1;
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
-
+            else if(newSpeed>0 && newSpeed<=50){
+                changeGear(1);
+            }
+            else if(newSpeed>50 && newSpeed<=100){
+                changeGear(2);
+            }
+            else if(newSpeed>100 && newSpeed<=150){
+                changeGear(3);
+            }
+            else if(newSpeed>150 && newSpeed<=200){
+                changeGear(4);
+            }
+            else if(newSpeed>200 && newSpeed<=250){
+                changeGear(5);
+            }
+            else{
+                changeGear(6);
+            }
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
